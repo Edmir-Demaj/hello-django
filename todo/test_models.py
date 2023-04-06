137d1b9e8c03c91350883059bad627de2568a712
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-04-06 10:32:09 +0000

    Create covereage for our code testings

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,14 +1,20 @@
 from django.test import TestCase
 # class TEstCase is an extension of the python standart
 # library module called unit tests
+from .models import Item
 
 # Create your tests here.
+# here we will test that our todo items will be created by default
+#  with the done status of false
 
 
-class TestDjango(TestCase):
+class TestModels(TestCase):
     # inside our class every test will be defined as a method
 
-    def test_this_works(self):
-        # this built-in method determine if 1==0
-        self.assertEqual(1, 1)
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
 
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
